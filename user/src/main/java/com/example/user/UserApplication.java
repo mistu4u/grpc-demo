7e53dcbdae4e5bbf2bfdb7babaccba6ac6afbd66
adhikarisubir@gmail.com <74544776+subiradhikari@users.noreply.github.com>
2021-05-03 09:48:51 -0500

    Component scan fixed

--- user/src/main/java/com/example/user/UserApplication.java
+++ user/src/main/java/com/example/user/UserApplication.java
@@ -1,22 +1,17 @@
 package com.example.user;
 
 import com.example.user.grpc.GrpcImpl;
-import com.example.user.service.UserService;
 import com.example.user.service.UserServiceImpl;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.io.IOException;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.user.grpc"})
+@ComponentScan(basePackages = {"com.example.user.grpc","com.example.user"})
 public class UserApplication {
   public static void main(String[] args) throws IOException, InterruptedException {
     SpringApplication.run(UserApplication.class, args);
--- user/src/main/java/com/example/user/api/GetUser.java
+++ user/src/main/java/com/example/user/api/GetUser.java
@@ -6,21 +6,20 @@ import com.example.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class GetUser {
   private UserService userService;
 
   @Autowired
-  GetUser(UserService userService){
+  GetUser(UserService userService) {
     this.userService = userService;
   }
 
-  @RequestMapping(path = "{id}")
+  @GetMapping("/users/{id}")
   public ResponseEntity<User> getUser(@PathVariable("id") int id) throws UserException {
     User user = userService.getUser(id);
     return new ResponseEntity<>(user, HttpStatus.OK);
